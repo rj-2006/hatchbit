ec2eeeaf87fa663bf6df656bb6477d43d551350f
rj-2006 <keturahu295@gmail.com>
2025-12-26 00:29:27 +0530

    background script

--- src/background.js
+++ src/background.js
@@ -11,3 +11,49 @@ chrome.runtime.onInstalled.addListener( ()=> {
     chrome.storage.local.set({pokedata: init_data});
     console.log("Initialized local storage for Pokebit.")
 })
+// Creating the alarm
+chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === "pomotimer"){
+        handleTimerComplete();
+    }
+});
+//Starting the Pomodoro
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>{
+    if (message.command === "startTimer"){
+        chrome.alarms.create("pomotimer", {delayInMinutes: message.minutes});
+
+        console.log(`Timer started for ${message.minutes} minutes.`);
+        sendResponse({status: "timer_start"});
+    }
+});
+
+async function handleTimerComplete() {
+    const result = await chrome.storage.local.get(["pokedata"]);
+    if(!result.pokedata) return;
+
+    let { stats, activePokemon} = result.pokedata;
+
+    stats.xp += 50;
+
+    if (stats.xp >= xpToNextLevel){
+        stats.level += 1;
+        stats.xp = 0;
+        stats.xpToNextLevel = Math.floor(stats.xpToNextLevel * 1.75);
+
+        showNotification("Level Up!", `${activePokemon} reached level ${stats.level}`);
+    }else{
+        showNotification("Congrats on finishing the pomodoro session!");
+    }
+
+    await chrome.local.storage.local.set({pokedata : {...result.pokedata, stats}});
+}
+
+function showNotification(title,message) {
+    chrome.notifications.create({
+        type: "basic",
+        iconUrl: "src/assets/icon.jpg",
+        title: title,
+        message: message,
+        priority: 2
+    });
+}
--- src/constants/themes.js
+++ src/constants/themes.js
@@ -1,3 +1,22 @@
-export const PKMN_THEMES = [
-    
-]
+export const PKMN_THEMES = {
+    bulbasaur: {
+        primary: "#399494",
+        secondary : "#62d5b4",
+        bg: "#73ac31"
+    },
+    charmander: {
+        primary: "#ff9441" ,
+        secondary: "#de5239",
+        bg: "#8b2900"
+    },
+    squirtle: {
+        primary: "#5a9ca4" ,
+        secondary: "#8bc5cd",
+        bg: "#297383"
+    },
+    pikachu: {
+        primary: "#f6e652" ,
+        secondary: "#f6bd20",
+        bg: "#9c5200"
+    }
+}
