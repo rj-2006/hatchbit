d0ea98e548c0556d7399ef4a4bd498ba52cd77c8
rj-2006 <keturahu295@gmail.com>
2025-12-29 15:33:39 +0530

    basic frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import { useState } from 'react'
+import React,  { useState, useEffect } from 'react'
+import { PKMN_THEMES } from './constants/themes'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokeData, setPokeData] = useState(null);
+  const [timeLeft, setTimeLeft] = useState(25 * 60); 
+
+  useEffect(() => {
+    chrome.storage.local.get(["pokedata"], (result) => {
+      if (result.pokedata) setPokeData(result.pokedata);
+    });
+
+    const listener = (changes) => {
+      if (changes.pokedata) {
+        setPokeData(changes.pokedata.newValue);
+      }
+    };
+    chrome.storage.onChanged.addListener(listener);
+    return () => chrome.storage.onChanged.removeListener(listener);
+  }, []);
+
+  const startPomo = () =>{
+    chrome.runtime.sendMessage({command: "startTimer", minutes: 25}, (response) => {
+      console.log(response.status);
+    });
+  };
+
+  if (!pokeData) return <div>Loading...</div>;
+
+  const currentTheme = PKMN_THEMES[pokedata.activePokemon];
 
   return (
     <>
-    <div>
-      Hatchbit
-    </div>
+
     </>
   )
-}
+  }
 
-export default App
+
+export default App;
